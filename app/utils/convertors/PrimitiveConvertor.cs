d9a53c9da0cf74a63437cfd5dfa247caeae2f8ac
Igor Kalinka <igorkalinka@Igors-MacBook-Pro-3.local>
2017-02-03 20:04:35 +0200

    return added

--- app/utils/convertors/PrimitiveConvertor.cs
+++ app/utils/convertors/PrimitiveConvertor.cs
@@ -5,23 +5,26 @@ namespace csharpcore
 	{
 		int intVar;
 		char charVar;
-		public void floatToChar(float floatVar)
+		public char floatToChar(float floatVar)
 		{
 			charVar = (char) floatVar;
 			Console.WriteLine("Input float value is " + floatVar
 			+ ". Output char value is " + charVar);
+			return charVar;
 		}
-		public void intToChar(int intVar)
+		public char intToChar(int intVar)
 		{
 			charVar = (char)intVar;
 			Console.WriteLine("Input int value is " + intVar
 			+ ". Output char value is " + charVar);
+			return charVar;
 		}
-		public void charToInt(char charVar)
+		public int charToInt(char charVar)
 		{
 			intVar = (int) charVar;
 			Console.WriteLine("Input char value is " + charVar
 			+ ". Output int value is " + intVar);
+			return intVar;
 		}
 	}
 }
